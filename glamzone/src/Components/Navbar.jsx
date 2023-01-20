4e909f87be96311314ba7de9ae860d6498030ced
Rushijoshi3798 <rushijoshi3798@gmail.com>
2023-01-20 15:10:24 +0530

    landing

--- glamzone/src/Components/Navbar.jsx
+++ glamzone/src/Components/Navbar.jsx
@@ -19,7 +19,7 @@ import {
   SearchIcon,
   TriangleDownIcon,
 } from "@chakra-ui/icons";
-import {MdLocationOn , } from "react-icons/md";
+import { MdLocationOn } from "react-icons/md";
 import { BsFillBagCheckFill } from "react-icons/bs";
 
 const Navbar = () => {
@@ -63,121 +63,185 @@ const Navbar = () => {
         </HStack>
 
         <HStack>
-        <Flex >
-          <Menu>
-            <MenuButton
-            
-              // border={"1px solid black"}
-              background="none"
-              borderRadius={"none"}
-              _hover={{ borderBottom: "2px solid black" }}
-              as={Button}
-              fontFamily={"Roboto, Helvetica, Arial, sans-serif"}
-              fontSize={"13px"}
-              color="grey.200"
-              rightIcon={<TriangleDownIcon />}>
-              <HStack>
-                <Avatar name={``} size="sm" src="https://bit.ly/broken-link" />
-                <Text fontStyle={""} fontFamily={"Roboto, Helvetica, Arial, sans-serif"} fontSize={"13px"}>Sign in</Text>
-              </HStack>
-            </MenuButton>
-            <MenuList
-              boxShadow={{
-                sm: "none",
-                md: "none",
-                lg: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
-              }}
-              border={{
-              sm: "none",
-              md: "none",
-              lg: "none"}}
-              w={["100px", "250px", "200px"]}>
-              <MenuItem
-                _hover={{ textDecoration: "underline", background: "none" }}>
-                <Box
-                  paddingY={1}
-                  paddingX={2}
-                  fontSize={"14px"}
-                  color="#000000"
-                  fontFamily={"Roboto, Helvetica, Arial, sans-serif;"}
-                  w={["100px", "150px", "200px"]}>
-                  Sign In
-                </Box>
-                <ChevronRightIcon />
-              </MenuItem>
+          <Flex>
+            <Menu>
+              <MenuButton
+                // border={"1px solid black"}
+                background="none"
+                borderRadius={"none"}
+                _hover={{ borderBottom: "2px solid black" }}
+                as={Button}
+                fontFamily={"Roboto, Helvetica, Arial, sans-serif"}
+                fontSize={"13px"}
+                color="grey.200"
+                rightIcon={<TriangleDownIcon />}>
+                <HStack>
+                  <Avatar
+                    name={``}
+                    size="sm"
+                    src="https://bit.ly/broken-link"
+                  />
+                  <Text
+                    fontStyle={""}
+                    fontFamily={"Roboto, Helvetica, Arial, sans-serif"}
+                    fontSize={"13px"}>
+                    Sign in
+                  </Text>
+                </HStack>
+              </MenuButton>
+              <MenuList
+                boxShadow={{
+                  sm: "none",
+                  md: "none",
+                  lg: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
+                }}
+                border={{
+                  sm: "none",
+                  md: "none",
+                  lg: "none",
+                }}
+                w={["100px", "250px", "200px"]}>
+                <MenuItem
+                  _hover={{ textDecoration: "underline", background: "none" }}>
+                  <Box
+                    paddingY={1}
+                    paddingX={2}
+                    fontSize={"14px"}
+                    color="#000000"
+                    fontFamily={"Roboto, Helvetica, Arial, sans-serif;"}
+                    w={["100px", "150px", "200px"]}>
+                    Sign In
+                  </Box>
+                  <ChevronRightIcon />
+                </MenuItem>
 
-              <MenuItem
-                _hover={{ textDecoration: "underline", background: "none" }}>
-                <Box
-                  paddingY={1}
-                  paddingX={2}
-                  fontSize={"14px"}
-                  color="#000000"
-                  fontFamily={"Roboto, Helvetica, Arial, sans-serif;"}
-                  w={["100px", "150px", "200px"]}>
-                  My orders
-                </Box>
-                <ChevronRightIcon />
-              </MenuItem>
+                <MenuItem
+                  _hover={{ textDecoration: "underline", background: "none" }}>
+                  <Box
+                    paddingY={1}
+                    paddingX={2}
+                    fontSize={"14px"}
+                    color="#000000"
+                    fontFamily={"Roboto, Helvetica, Arial, sans-serif;"}
+                    w={["100px", "150px", "200px"]}>
+                    My orders
+                  </Box>
+                  <ChevronRightIcon />
+                </MenuItem>
 
-              <MenuItem
-                _hover={{ textDecoration: "underline", background: "none" }}>
-                <Box
-                  paddingY={1}
-                  paddingX={2}
-                  fontSize={"14px"}
-                  color="#000000"
-                  fontFamily={"Roboto, Helvetica, Arial, sans-serif;"}
-                  w={["100px", "150px", "200px"]}>
-                  My Favourite
-                </Box>
-                <ChevronRightIcon />
-              </MenuItem>
+                <MenuItem
+                  _hover={{ textDecoration: "underline", background: "none" }}>
+                  <Box
+                    paddingY={1}
+                    paddingX={2}
+                    fontSize={"14px"}
+                    color="#000000"
+                    fontFamily={"Roboto, Helvetica, Arial, sans-serif;"}
+                    w={["100px", "150px", "200px"]}>
+                    My Favourite
+                  </Box>
+                  <ChevronRightIcon />
+                </MenuItem>
 
-              <MenuItem
-                _hover={{ textDecoration: "underline", background: "none" }}>
-                <Box
-                  paddingY={1}
-                  paddingX={2}
-                  fontSize={"14px"}
-                  color="#000000"
-                  fontFamily={"Roboto, Helvetica, Arial, sans-serif;"}
-                  w={["100px", "150px", "200px"]}>
-                  My VIP Rewards
-                </Box>
-                <ChevronRightIcon />
-              </MenuItem>
-            </MenuList>
-          </Menu>
-          
-        </Flex>
-        <Flex 
-          marginLeft={"0px"}
-          // border={"1px solid black"}
-          background="none"
-          borderRadius={"none"}
-          paddingY={2.5}
-          paddingX={2}
-          _hover={{ borderBottom: "2px solid black" }}>
-          <MdLocationOn />{" "}
-          <Text marginInlineStart={"5px"} fontSize={"14px"}>Find a Store</Text>
-        </Flex>
+                <MenuItem
+                  _hover={{ textDecoration: "underline", background: "none" }}>
+                  <Box
+                    paddingY={1}
+                    paddingX={2}
+                    fontSize={"14px"}
+                    color="#000000"
+                    fontFamily={"Roboto, Helvetica, Arial, sans-serif;"}
+                    w={["100px", "150px", "200px"]}>
+                    My VIP Rewards
+                  </Box>
+                  <ChevronRightIcon />
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+          <Flex
+            marginLeft={"0px"}
+            // border={"1px solid black"}
+            background="none"
+            borderRadius={"none"}
+            paddingY={2.5}
+            paddingX={2}
+            _hover={{ borderBottom: "2px solid black" }}>
+            <MdLocationOn />{" "}
+            <Text marginInlineStart={"5px"} fontSize={"14px"}>
+              Find a Store
+            </Text>
+          </Flex>
 
-        <Flex
-        marginLeft={"0px"}
-        // border={"1px solid black"}
-        background="none"
-        borderRadius={"none"}
-        paddingY={2.5}
-        paddingX={2}
-        _hover={{ borderBottom: "2px solid black" }}>
-          <BsFillBagCheckFill />
-          <Text marginInlineStart={"10px"} fontSize={"14px"}>Bag</Text>
-        </Flex>
-       
+          <Flex
+            marginLeft={"0px"}
+            // border={"1px solid black"}
+            background="none"
+            borderRadius={"none"}
+            paddingY={2.5}
+            paddingX={2}
+            _hover={{ borderBottom: "2px solid black" }}>
+            <BsFillBagCheckFill />
+            <Text marginInlineStart={"10px"} fontSize={"14px"}>
+              Bag
+            </Text>
+          </Flex>
         </HStack>
       </Flex>
-      <Box></Box>
+      <hr></hr>
+
+      <Flex justifyContent={"space-around"}>
+        <HStack
+          fontSize={"14px"}
+          color={"#000000"}
+          fontFamily={"Roboto, Helvetica, Arial, sans-serif;"}
+          h="10px"
+          p={4}>
+          <Flex
+            _hover={{ cursor: "pointer", borderBottom: "2px solid black" }}
+            p={2}
+            px={4}>
+            Women
+          </Flex>
+          <Flex
+            _hover={{ cursor: "pointer", borderBottom: "2px solid black" }}
+            p={2}
+            px={4}>
+            Men
+          </Flex>
+          <Flex
+            _hover={{ cursor: "pointer", borderBottom: "2px solid black" }}
+            p={2}
+            px={4}>
+            Kids
+          </Flex>
+          <Flex
+            _hover={{ cursor: "pointer", borderBottom: "2px solid black" }}
+            p={2}
+            px={4}>
+            Athletic & Snekears
+          </Flex>
+          <Flex
+            _hover={{ cursor: "pointer", borderBottom: "2px solid black" }}
+            p={2}
+            px={4}>
+            Brands
+          </Flex>
+          <Flex
+            _hover={{ cursor: "pointer", borderBottom: "2px solid black" }}
+            p={2}
+            px={4}>
+            Clearance
+          </Flex>
+          <Flex
+            _hover={{ cursor: "pointer", borderBottom: "2px solid black" }}
+            p={2}
+            px={4}>
+            Deals
+          </Flex>
+        </HStack>
+      </Flex>
+      <hr></hr>
     </div>
   );
 };
--- glamzone/src/Pages/Home.jsx
+++ glamzone/src/Pages/Home.jsx
@@ -1,13 +1,104 @@
-import React from 'react'
-import Navbar from '../Components/Navbar';
+import React from "react";
+import Navbar from "../Components/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      Home
+      <div style={{width: "100%"}}>
+        <div
+          style={{
+            position: "relative",
+            width: "100%",
+          }}>
+          <img
+            src="https://images.dsw.com/is/image/DSWShoes/P222162_ft_boots?impolicy=qlt-medium&imwidth=1400&imdensity=1"
+            alt="Snow"
+            style={{ width: "100%", height: "600px" }}
+          />
+          <p
+            style={{
+              border: "none",
+              position: "absolute",
+              top: "30%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              MediaStreamTransform: "translate(-50%, -50%)",
+              color: "white",
+              fontWeight: "bolder",
+              fontFamily: "sans-serif",
+              fontSize: "40px",
+              padding: "12px 20px",
+              width: "35%",
+              borderRadius: "5px",
+              textAlign: "center",
+            }}>
+            Need-Now Boots Under $100
+          </p>
+          <p
+            style={{
+              border: "none",
+              position: "absolute",
+              top: "42%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              MediaStreamTransform: "translate(-50%, -50%)",
+              color: "white",
+              fontWeight: "bolder",
+              fontFamily: "sans-serif",
+              fontSize: "14px",
+              padding: "12px 20px",
+              width: "35%",
+              borderRadius: "5px",
+              textAlign: "center",
+              letterSpacing: "0.1em",
+            }}>
+            Your Kind of Boots at Your kind of Prices
+          </p>
+          <button
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "43%",
+              transform: "translate(-50%, -50%)",
+              MediaStreamTransform: "translate(-50%, -50%)",
+              backgroundColor: "#fff",
+              color: "black",
+              fontSize: "14px",
+              fontWeight: "bolder",
+              padding: "12px 45px",
+              border: "none",
+              cursor: "pointer",
+              borderRadius: "5px",
+              textAlign: "center",
+              textDecoration: "underline"
+            }}>
+            SHOP WOMEN'S
+          </button>
+          <button
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "57%",
+              transform: "translate(-50%, -50%)",
+              MediaStreamTransform: "translate(-50%, -50%)",
+              backgroundColor: "#fff",
+              color: "black",
+              fontSize: "14px",
+              fontWeight: "bolder",
+              padding: "12px 50px",
+              border: "none",
+              cursor: "pointer",
+              borderRadius: "5px",
+              textAlign: "center",
+              textDecoration: "underline"
+            }}>
+            SHOP MEN'S
+          </button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
