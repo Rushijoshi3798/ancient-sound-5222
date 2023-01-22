40cbf0a8371cbf5fadf75554e0a8e62671b39ad0
Rushijoshi3798 <rushijoshi3798@gmail.com>
2023-01-22 15:09:35 +0530

    navbar updated

--- glamzone/src/Pages/Home.jsx
+++ glamzone/src/Pages/Home.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import Navbar from '../Components/Navbar';
+import {
+  Box,
+  
+  
+  Grid,
+  GridItem,
+  Image,
+ 
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+import Navbar from "../Components/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <div style={{width: "100%"}}>
+      <div style={{marginTop: "50px", width: "100%" }}>
         <div
           style={{
             position: "relative",
@@ -71,7 +81,7 @@ const Home = () => {
               cursor: "pointer",
               borderRadius: "5px",
               textAlign: "center",
-              textDecoration: "underline"
+              textDecoration: "underline",
             }}>
             SHOP WOMEN'S
           </button>
@@ -91,16 +101,249 @@ const Home = () => {
               cursor: "pointer",
               borderRadius: "5px",
               textAlign: "center",
-              textDecoration: "underline"
+              textDecoration: "underline",
             }}>
             SHOP MEN'S
           </button>
         </div>
       </div>
 
-      
+      <Box marginTop={"50px"}>
+        <Text fontSize={"32px"} fontWeight={"bold"}>
+          The Deals To Know
+        </Text>
+        <Grid
+          h={"auto"}
+          px={4}
+          templateColumns={[
+            "repeat(1, 1fr)",
+            "repeat(2, 1fr)",
+            "repeat(4, 1fr)",
+          ]}
+          gap={4}>
+          <GridItem w="100%">
+            <Image
+              cursor={"pointer"}
+              src={
+                "https://images.dsw.com/is/image/DSWShoes/P222162_editorial_boot-1?impolicy=qlt-medium&imwidth=1011&imdensity=1"
+              }
+            />
+            <Text
+              fontSize="14px"
+              marginTop={"5px"}
+              fontWeight={"bold"}
+              textDecoration="underline"
+              cursor={"pointer"}>
+              Shop Now
+            </Text>
+          </GridItem>
+          <GridItem w="100%">
+            <Image
+              cursor={"pointer"}
+              src={
+                "https://images.dsw.com/is/image/DSWShoes/P222162_editorial_boot-5?impolicy=qlt-medium&imwidth=1011&imdensity=1"
+              }
+            />
+            <Text
+              fontSize="14px"
+              marginTop={"5px"}
+              fontWeight={"bold"}
+              textDecoration="underline"
+              cursor={"pointer"}>
+              Shop Now
+            </Text>
+          </GridItem>
+          <GridItem w="100%">
+            <Image
+              cursor={"pointer"}
+              src={
+                "https://images.dsw.com/is/image/DSWShoes/P222162_editorial_boot-6?impolicy=qlt-medium&imwidth=1011&imdensity=1"
+              }
+            />
+            <Text
+              fontSize="14px"
+              marginTop={"5px"}
+              fontWeight={"bold"}
+              textDecoration="underline"
+              cursor={"pointer"}>
+              Shop Now
+            </Text>
+          </GridItem>
+          <GridItem w="100%">
+            <Image
+              cursor={"pointer"}
+              src={
+                "https://images.dsw.com/is/image/DSWShoes/P222162_editorial_boot-7-NEW.jpg?impolicy=qlt-medium&imwidth=1011&imdensity=1"
+              }
+            />
+            <Text
+              fontSize="14px"
+              marginTop={"5px"}
+              fontWeight={"bold"}
+              textDecoration="underline"
+              cursor={"pointer"}>
+              Shop Now
+            </Text>
+          </GridItem>
+        </Grid>
+      </Box>
+
+      <Box marginTop={"60px"} marginBottom={"60px"}>
+        <Grid
+          textAlign={"center"}
+          h={"auto"}
+          px={14}
+          py={5}
+          templateColumns={[
+            "repeat(1, 1fr)",
+            "repeat(2, 1fr)",
+            "repeat(3, 1fr)",
+          ]}
+          gap={4}>
+          <GridItem w="100%">
+            <Image
+              cursor={"pointer"}
+              src={
+                "https://images.dsw.com/is/image/DSWShoes/P222120_editorial_w-lug?impolicy=qlt-medium&imwidth=1011&imdensity=1"
+              }
+            />
+            <Text
+              fontSize="16px"
+              marginTop={"5px"}
+              fontWeight={"bold"}
+              cursor={"pointer"}>
+              Your New Favorite Lug-Sole Boots
+            </Text>
+          </GridItem>
+          <GridItem w="100%">
+            <Image
+              cursor={"pointer"}
+              src={
+                "https://images.dsw.com/is/image/DSWShoes/P222120_editorial_w-booties?impolicy=qlt-medium&imwidth=1011&imdensity=1"
+              }
+            />
+            <Text
+              fontSize="16px"
+              marginTop={"5px"}
+              fontWeight={"bold"}
+              cursor={"pointer"}>
+              Dress-Up-or-Down Booties
+            </Text>
+          </GridItem>
+          <GridItem w="100%">
+            <Image
+              cursor={"pointer"}
+              src={
+                "https://images.dsw.com/is/image/DSWShoes/P222120_editorial_w-platform?impolicy=qlt-medium&imwidth=1011&imdensity=1"
+              }
+            />
+            <Text
+              fontSize="16px"
+              marginTop={"5px"}
+              fontWeight={"bold"}
+              cursor={"pointer"}>
+              Outfit-Elevating Platform Boots
+            </Text>
+          </GridItem>
+        </Grid>
+      </Box>
+
+      <div style={{ marginTop: "20px", width: "100%" }}>
+        <div
+          style={{
+            position: "relative",
+            width: "100%",
+          }}>
+          <img
+            src="https://images.dsw.com/is/image/DSWShoes/P222150_ft_sneakers.jpg?impolicy=qlt-medium&imwidth=1400&imdensity=1"
+            alt="Snow"
+            style={{ width: "100%", height: "600px" }}
+          />
+          <p
+            style={{
+              border: "none",
+              position: "absolute",
+              top: "45%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              MediaStreamTransform: "translate(-50%, -50%)",
+              color: "white",
+              fontWeight: "bolder",
+              fontFamily: "sans-serif",
+              fontSize: "36px",
+              padding: "12px 20px",
+              width: "35%",
+              borderRadius: "5px",
+              textAlign: "center",
+            }}>
+            On-Trend Sneakers Under $75
+          </p>
+          <p
+            style={{
+              border: "none",
+              position: "absolute",
+              top: "55%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              MediaStreamTransform: "translate(-50%, -50%)",
+              color: "white",
+              fontWeight: "bolder",
+              fontFamily: "sans-serif",
+              fontSize: "14px",
+              padding: "12px 20px",
+              width: "35%",
+              borderRadius: "5px",
+              textAlign: "center",
+              letterSpacing: "0.1em",
+            }}>
+            These have “You” written all over them.
+          </p>
+          <button
+            style={{
+              position: "absolute",
+              top: "65%",
+              left: "43%",
+              transform: "translate(-50%, -50%)",
+              MediaStreamTransform: "translate(-50%, -50%)",
+              backgroundColor: "#fff",
+              color: "black",
+              fontSize: "14px",
+              fontWeight: "bolder",
+              padding: "12px 45px",
+              border: "none",
+              cursor: "pointer",
+              borderRadius: "5px",
+              textAlign: "center",
+              textDecoration: "underline",
+            }}>
+            SHOP WOMEN'S
+          </button>
+          <button
+            style={{
+              position: "absolute",
+              top: "65%",
+              left: "57%",
+              transform: "translate(-50%, -50%)",
+              MediaStreamTransform: "translate(-50%, -50%)",
+              backgroundColor: "#fff",
+              color: "black",
+              fontSize: "14px",
+              fontWeight: "bolder",
+              padding: "12px 50px",
+              border: "none",
+              cursor: "pointer",
+              borderRadius: "5px",
+              textAlign: "center",
+              textDecoration: "underline",
+            }}>
+            SHOP MEN'S
+          </button>
+        </div>
+      </div>
+
+      <Box></Box>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
